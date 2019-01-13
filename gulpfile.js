be4468a89ab48bb5d00c44dcecd45c40a71179a4
Karimmbk <k171395@g.unicamp.br>
2019-01-13 20:48:02 -0200

    SAVEPOINT

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,39 @@ const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
+gulp.task('directories', () => gulp
+  .src('*.*', { read: false })
+  .pipe(gulp.dest('./css/scss'))
+  .pipe(gulp.dest('./img/content'))
+  .pipe(gulp.dest('./img/icons'))
+  .pipe(gulp.dest('./js/main')));
+
 // Funçao para compilar o SASS e adicionar os prefixos
 function compilaSass() {
   return gulp
-  .src('css/scss/**/*.scss')
-  .pipe(sass({ // comprimir os arquivos sass no style.css
-    outputStyle: 'expanded'
-  }))
-  .pipe(autoprefixer({ // adicionar os prefixos de (-webkit, ...)
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(gulp.dest('css/')) // salvar as mudanças dos arquivos Sass na pasta css
-  .pipe(browserSync.stream()) // auto reload no browser para fazer as mudanças na hora
+    .src('css/scss/**/*.scss')
+    .pipe(
+      sass({
+        // comprimir os arquivos sass no style.css
+        outputStyle: 'compressed',
+      }),
+    )
+    .pipe(
+      autoprefixer({
+        // adicionar os prefixos de (-webkit, ...)
+        browsers: ['last 2 versions'],
+        cascade: false,
+      }),
+    )
+    .pipe(gulp.dest('css/')) // salvar as mudanças dos arquivos Sass na pasta css
+    .pipe(browserSync.stream()); // auto reload no browser para fazer as mudanças na hora
 }
 
 // Tarefa de gulp para a função de SASS
-gulp.task('sass', function(done) { // se eu for chamar alguma funcao com parametro, precisa-se chamar o task com uma funcao anonima e dentro dela chamar a funcao desejada. Prescisa-se tb colocor esse parametro 'done', para saber que acabou
+gulp.task('sass', (done) => {
+  /* se eu for chamar alguma funcao com parametro, precisa-se chamar o task com uma
+     funcao anonima e dentro dela chamar a funcao desejada. Prescisa-se tb colocor esse
+     parametro 'done', para saber que acabou */
   compilaSass();
   done();
 });
@@ -31,14 +47,17 @@ gulp.task('sass', function(done) { // se eu for chamar alguma funcao com paramet
 // Função para juntar os arquivos js em um só
 function gulpJS() {
   return gulp
-  .src('js/main/**/*.js')
-  .pipe(concat('main.js')) //juntar os arquivos js em um
-  .pipe(babel({ // serve para transformar o js para ser compativel com browser's mais antigos
-    presets: ['env']
-  }))
-  .pipe(uglify())
-  .pipe(gulp.dest('js/')) // onde vai ser salvo o arquivo final
-  .pipe(browserSync.stream())
+    .src('js/main/**/*.js')
+    .pipe(concat('main.js')) // juntar os arquivos js em um
+    .pipe(
+      babel({
+        // serve para transformar o js para ser compativel com browser's mais antigos
+        presets: ['env'],
+      }),
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest('js/')) // onde vai ser salvo o arquivo final
+    .pipe(browserSync.stream());
 }
 
 gulp.task('mainjs', gulpJS);
@@ -46,22 +65,21 @@ gulp.task('mainjs', gulpJS);
 // JS Plugins
 function pluginJS() {
   return gulp
-  .src([
-    'js/plugins/*.js'
-  ])
-  .pipe(concat('plugins.js'))
-  .pipe(gulp.dest('js/'))
-  .pipe(browserSync.stream())
+    .src(['js/plugins/*.js'])
+    .pipe(concat('plugins.js'))
+    .pipe(gulp.dest('js/'))
+    .pipe(browserSync.stream());
 }
 
 gulp.task('pluginjs', pluginJS);
 
 // Função para iniciar o browser
-function browser() { // abrir o browser com local host
+function browser() {
+  // abrir o browser com local host
   browserSync.init({
     server: {
-      baseDir: "./"
-    }
+      baseDir: './',
+    },
   });
 }
 
@@ -79,4 +97,4 @@ function watch() {
 gulp.task('watch', watch);
 
 // Tarefa padrão do Gulp, que inicia o watch e o browser-sync
-gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'mainjs'));
+gulp.task('default', gulp.parallel('directories', 'watch', 'browser-sync', 'sass', 'mainjs'));
